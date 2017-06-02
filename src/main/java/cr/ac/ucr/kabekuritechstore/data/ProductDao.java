55875366e42c5ab1d72c8de9737611ce4beb4576
FernandoSolano <fer_odi10@hotmail.com>
2017-06-02 12:50:14 -0600

    Category CRUD functionality added

--- src/main/java/cr/ac/ucr/kabekuritechstore/data/ProductDao.java
+++ src/main/java/cr/ac/ucr/kabekuritechstore/data/ProductDao.java
@@ -2,6 +2,7 @@ package cr.ac.ucr.kabekuritechstore.data;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,18 +14,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import cr.ac.ucr.kabekuritechstore.domain.Product;
 
 @Repository
 public class ProductDao {
 	private JdbcTemplate jdbcTemplate;
+	private SimpleJdbcCall simpleJdbcCall;
 	
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.simpleJdbcCall= new SimpleJdbcCall(dataSource)				
+				.withProcedureName("InsertarLibro")
+				.withoutProcedureColumnMetaDataAccess()
+				.declareParameters(new SqlOutParameter("@numLibro", Types.INTEGER))
+				.declareParameters(new SqlParameter("@tituloLibro",Types.VARCHAR))
+				.declareParameters(new SqlParameter("@anoPublicacion",Types.INTEGER))
+				.declareParameters(new SqlParameter("@isbn",Types.VARCHAR))
+				.declareParameters(new SqlParameter("@codPublicador",Types.INTEGER))
+				.declareParameters(new SqlParameter("@precio",Types.FLOAT));
 	}
 	
 	public List<Product> findAll(){
@@ -38,10 +54,7 @@ public class ProductDao {
 	}
 	
 	public List<Product> findProductsByName(String product_name){
-		String sqlSelect = "SELECT p.id, p.name, p.image_url, p.description,"
-				+ " p.price, p.units_on_stock, c.id AS category_id, c.name AS category_name"
-				+ " FROM products p, categories c"
-				+ " WHERE p.category_id=c.id AND p.name LIKE '%"+product_name+"%'";
+		String sqlSelect = "CALL sp_product_get_by_name ('"+product_name+"')";
 		return jdbcTemplate.query(sqlSelect, new findAllProductsExtractor());
 	}
 	
@@ -62,6 +75,32 @@ public class ProductDao {
 			return products.get(0);
 		}
 	}
+	
+	/*@Transactional
+	public Libro save(Libro libro) throws SQLException{
+		SqlParameterSource parameterSource = new MapSqlParameterSource()
+				.addValue("@tituloLibro", libro.getTituloLibro())
+				.addValue("@anoPublicacion", libro.getAnoPublicacion())						
+				.addValue("@isbn", libro.getIsbn())	
+				.addValue("@codPublicador", libro.getPublicador().getCodPublicador())
+				.addValue("@precio",libro.getPrecio());
+		Map<String, Object> outParameter = simpleJdbcCallLibro.execute(parameterSource);
+		libro.setNumLibro(Integer.parseInt((outParameter.get("@numLibro").toString())));
+		
+		for(Autor autor : libro.getAutores()){
+			insertLibroAutor(libro.getNumLibro(),autor.getCodAutor());
+		}
+			
+		return libro;
+	}
+	
+	@Transactional
+	public void insertLibroAutor(int codLibro,int codAutor) throws SQLException{
+		SqlParameterSource parameterSource = new MapSqlParameterSource()
+				.addValue("@numLibro", codLibro)
+				.addValue("@codAutor", codAutor);
+		simpleJdbcAutor.execute(parameterSource);
+	}*/
 	
 	private static final class findAllProductsExtractor implements ResultSetExtractor<List<Product>> {
 		@Override
--- src/main/java/cr/ac/ucr/kabekuritechstore/form/CategoryForm.java
+++ src/main/java/cr/ac/ucr/kabekuritechstore/form/CategoryForm.java
@@ -1,27 +1,46 @@
-package cr.ac.ucr.kabekuritechstore.domain;
+package cr.ac.ucr.kabekuritechstore.form;
 
-public class Category {
+import javax.validation.constraints.Size;
+
+
+
+public class CategoryForm {
+	
 	private int id;
+	
+	
+	@Size(min=2, max=50)
 	private String name;
-	
-	public Category() {
+
+	public CategoryForm() {
+		
 	}
+
 	
-	public Category(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
+
 	public int getId() {
 		return id;
 	}
+
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
+	
+	
+	
+
 }
